d375798a17047e19096266ffbdd6a16ec923f456
janvda <jan@Jans-MacBook-Pro.local>
2019-09-08 22:06:58 +0200

    added bluetooth connect to start script

--- pulseaudio-server/start.sh
+++ pulseaudio-server/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# export bluetooth_device_address="A0:E9:DB:09:CF:FF"
+
+if [ "$bluetooth_device_address" != '' ]; then
+   echo "connecting to bluetooth device with mac = $bluetooth_device_address"
+   echo -e 'connect $bluetooth_device_address' | bluetoothctl
+   echo -e 'info $bluetooth_device_address' | bluetoothctl
+   #echo -e 'disconnect $bluetooth_device_address' | bluetoothctl
+   echo "end of connecting"
+fi
+
 echo starting pulseaudio ...
 pulseaudio --log-level=4
 echo ERROR: pulseaudio stopped
