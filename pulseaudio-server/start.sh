1ee8a863b6627e374b259a2cb1bcdc6852c7b5ab
janvda <jan@Jans-MBP.lan>
2019-09-08 23:18:48 +0200

    created connect script

--- pulseaudio-server/start.sh
+++ pulseaudio-server/start.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# export bluetooth_device_address="A0:E9:DB:09:CF:FF"
-
-if [ "$bluetooth_device_address" != '' ]; then
-   echo "sleeping 40 sec..."
-   sleep 20
-   echo "connecting to bluetooth device with mac = $bluetooth_device_address"
-   echo -e 'connect $bluetooth_device_address' | bluetoothctl
-   sleep 20
-   echo -e 'info $bluetooth_device_address' | bluetoothctl
-   #echo -e 'disconnect $bluetooth_device_address' | bluetoothctl
-   echo "end of connecting"
-fi
-
 echo starting pulseaudio ...
 pulseaudio --log-level=4
 echo ERROR: pulseaudio stopped
