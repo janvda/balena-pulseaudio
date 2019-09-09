48f30dea686c56099ebaba87e835961ca157c1bd
janvda <jan@Jans-MacBook-Pro.local>
2019-09-09 13:26:18 +0200

    fix

--- pulseaudio-server/connect_bluetooth.sh
+++ pulseaudio-server/connect_bluetooth.sh
@@ -5,9 +5,9 @@ if [ "$bluetooth_device_address" != '' ]; then
    echo "start endless loop for connecting to bluetooth device with mac = $bluetooth_device_address"
    while (sleep 30)
    do
-      bluetoothctl <<- EOF > /dev/null
-         connect $bluetooth_device_address
-         exit
-      EOF
+     bluetoothctl <<- EOF > /dev/null
+        connect $bluetooth_device_address
+        exit
+EOF
    done
 fi
