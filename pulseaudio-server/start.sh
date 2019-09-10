3ecb8cff41d2408393a02671b8937559bf5c9d98
janvda <jan@Jans-MBP.lan>
2019-09-11 00:16:19 +0200

    added logic for env alsa-sink1 and also-source1

--- pulseaudio-server/start.sh
+++ pulseaudio-server/start.sh
@@ -2,6 +2,14 @@
 
 # TBD
 cp /etc/pulse/default_without_alsa_sinks_and_sources.pa /etc/pulse/default.pa
+echo "# adding alsa sinks/sources configured by env vars alsa-sinkN and alsa-sourceN :"
+if [ $alsa-sink1 != "" ]; then
+   echo "load-module module-alsa-sink $alsa-sink1" >> /etc/pulse/default.pa
+fi
+
+if [ $alsa-source1 != "" ]; then
+   echo "load-module module-alsa-source $alsa-source1" >> /etc/pulse/default.pa
+fi
 
 # run bluetooth connect loop
 /connect_bluetooth.sh &
