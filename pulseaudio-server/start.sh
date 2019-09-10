64b609b7348312b7d60e1b23fc4f6fc3f4820888
janvda <jan@Jans-MBP.lan>
2019-09-11 00:21:31 +0200

    fix

--- pulseaudio-server/start.sh
+++ pulseaudio-server/start.sh
@@ -2,13 +2,13 @@
 
 # TBD
 cp /etc/pulse/default_without_alsa_sinks_and_sources.pa /etc/pulse/default.pa
-echo "# adding alsa sinks/sources configured by env vars alsa-sinkN and alsa-sourceN :"
-if [ $alsa-sink1 != "" ]; then
-   echo "load-module module-alsa-sink $alsa-sink1" >> /etc/pulse/default.pa
+echo "# adding alsa sinks/sources configured by env vars alsa-sinkN and alsa-sourceN :" >> /etc/pulse/default.pa
+if [ "$alsa_sink1" != "" ]; then
+   echo "load-module module-alsa-sink $alsa_sink1" >> /etc/pulse/default.pa
 fi
 
-if [ $alsa-source1 != "" ]; then
-   echo "load-module module-alsa-source $alsa-source1" >> /etc/pulse/default.pa
+if [ "$alsa_source1" != "" ]; then
+   echo "load-module module-alsa-source $alsa_source1" >> /etc/pulse/default.pa
 fi
 
 # run bluetooth connect loop
