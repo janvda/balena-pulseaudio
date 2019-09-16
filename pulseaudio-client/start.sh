0d4837a7b00c3945a44c4b0b0244bf6158c282b4
janvda <jan@Jans-MBP.lan>
2019-09-16 21:00:55 +0200

    write test scenarios

--- pulseaudio-client/start.sh
+++ pulseaudio-client/start.sh
@@ -3,20 +3,37 @@
 # see https://github.com/janvda/balena-pulseaudio/issues/13
 unset DISPLAY
 
-echo -e "STEP 1: playing LRMonoPhase4.wav"
-paplay LRMonoPhase4.wav
+echo "Listing all sinks (playback devices)"
+pactl list sinks short
 
-echo -e "STEP 2: recording audio for 10 seconds "
-parecord --channels=1 record_session1.wav &
-sleep 10
-kill $!  #$! expands to the PID of the last process executed in the background
+echo "Listing all sources (audio capture devices)"
+pactl list sources
 
-echo -e "STEP 3: Playing recorded sample"
-paplay record_session1.wav
+case $test_id in
+"") ;;
+0) ;;
+1) echo "running test 1 - play audio file using paplay : "
+   paplay LRMonoPhase4.wav
+   echo "... end of test 1"
+   ;;
+2) echo "running test 2: "
+   echo "- Started recording audio for 10 seconds ..."
+   parecord --channels=1 record_session1.wav &
+   sleep 10
+   kill $!  #$! expands to the PID of the last process executed in the background
+   echo "- Playing recorded audio..."
+   paplay record_session1.wav
+   echo "... end of test 2"
+   ;;
+*) echo "\$test_id = $test_id is not a valid value".
+esac
 
-echo -e "STEP 4: Sleeping forever"
 while true; do
    # every hour a "zzz" is put into the log files.
    echo "    zzz"
    sleep 3600
 done
+
+
+
+echo -e "STEP 4: Sleeping forever"
