679746412ab25c328c1e62a52e6c2aba99aa712e
janvda <jan@Jans-MBP.lan>
2019-10-05 23:32:58 +0200

    intermediate checkin

--- pa-mqtt/main.py
+++ pa-mqtt/main.py
@@ -16,15 +16,26 @@ if __name__ == '__main__':
         mqttBroker= "mqtt" if ( os.environ.get("mqtt_broker") is None ) else os.environ["mqtt_broker"]
         mqttPort  =  1883  if ( os.environ.get("mqtt_port")   is None ) else int(os.environ["mqtt_port"])
 
+        def on_mqtt_connect(client, userdata, flags, rc):
+           print("Connected With Result Code "+rc)
+
+        def on_mqtt_message(client, userdata, message):
+           print("Message Recieved: "+message.payload.decode())
+
         mqttClient=mqtt.Client("pa-mqtt")
+        mqttClient.on_connect = on_mqtt_connect
+        mqttClient.on_message = on_mqtt_message
         mqttClient.connect(mqttBroker,mqttPort)
-        mqttClient.loop_start()
+
+        mqttClient.subscribe("pa-mqtt/cmd", qos=1)
 
         # Publish the details of the device we are listening to
         mqttClient.publish("pa-mqtt",
-                           "pa-mqtt service started at " + datetime.now(),
+                           "pa-mqtt service started at " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
                            2,    # qos= 2 - deliver exactly once
                            True) # tell broker to retain this message so that it gets delivered
+
+        mqttClient.loop_forever()
 
 
     # all exceptions are handled !
