b3a13973903a49d3673f3b7cd3bc279feb5ba52d
janvda <jan@Jans-MBP.lan>
2019-10-06 15:07:43 +0200

    renamed pa-mqtt into pulseaudio2mqtt

--- pulseaudio2mqtt/main.py
+++ pulseaudio2mqtt/main.py
@@ -2,15 +2,15 @@ import pulsectl, json, os, time
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
-pulse = pulsectl.Pulse('pa-mqtt')
+pulse = pulsectl.Pulse('pulseaudio2mqtt')
 
 
 if __name__ == '__main__':
     try:
-        print("Starting pa-mqtt service ...")
+        print("Starting pulseaudio2mqtt service ...")
 
         # connect to pulseaudio server
-        pulse = pulsectl.Pulse('pa-mqtt')
+        pulse = pulsectl.Pulse('pulseaudio2mqtt')
 
         # MQTT client setup
         mqttBroker= "mqtt" if ( os.environ.get("mqtt_broker") is None ) else os.environ["mqtt_broker"]
@@ -26,23 +26,23 @@ if __name__ == '__main__':
            print("MQTT_get_sinks message received: ["+ message.topic+"] "+str(message.payload))
            sinkList=pulse.sink_list()
            print(str(sinkList))
-           mqttClient.publish("pa-mqtt/cmd-rsp/get-sinks",
+           mqttClient.publish("pulseaudio2mqtt/cmd-rsp/get-sinks",
                            str(sinkList),
                            1,    # qos= 2 - deliver exactly once
                            False) # tell broker to retain this message so that it gets delivered
 
 
-        mqttClient=mqtt.Client("pa-mqtt")
+        mqttClient=mqtt.Client("pulseaudio2mqtt")
         mqttClient.on_connect = on_mqtt_connect # on_connect doesn't seem to work ??
         mqttClient.on_message = on_mqtt_message
         mqttClient.connect(mqttBroker,mqttPort)
 
-        mqttClient.subscribe("pa-mqtt/cmd/#", qos=1)
-        mqttClient.message_callback_add("pa-mqtt/cmd/get-sinks", on_mqtt_get_sinks)
+        mqttClient.subscribe("pulseaudio2mqtt/cmd/#", qos=1)
+        mqttClient.message_callback_add("pulseaudio2mqtt/cmd/get-sinks", on_mqtt_get_sinks)
 
         # Publish the details of the device we are listening to
-        mqttClient.publish("pa-mqtt",
-                           "pa-mqtt service started at " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+        mqttClient.publish("pulseaudio2mqtt",
+                           "pulseaudio2mqtt service started at " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
                            2,    # qos= 2 - deliver exactly once
                            True) # tell broker to retain this message so that it gets delivered
 
