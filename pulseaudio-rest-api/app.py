b7bdbc540a3673d73d44ed6aa79c773f0541d862
janvda <jan@Jans-MacBook-Pro.local>
2019-10-14 10:23:51 +0200

    added pulse help at root

--- pulseaudio-rest-api/app.py
+++ pulseaudio-rest-api/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request
 import pulsectl, os, time
 import jsonpickle
+import pydoc
 
 app = Flask(__name__)
 
@@ -9,8 +10,9 @@ app = Flask(__name__)
 pulse = pulsectl.Pulse('pulseaudio2mqtt')
 
 @app.route('/')
-def index():
-    return 'Index Page'
+def help():
+    pulseHelpHtml=pydoc.render_doc(pulse,renderer=pydoc.HTMLDoc())
+    return pulseHelpHtml
 
 @app.route('/server_info')
 def getServerInfo():
@@ -41,3 +43,9 @@ def getSinkInputList():
 def getSourceOutputList():
   pulsectlObject=pulse.source_output_list()
   return jsonpickle.encode(pulsectlObject)
+
+@app.route('/sink_volume_set/<index>', methods=['POST'])
+def getSinkVolumeSet(index):
+  volume = request.data # read volume from body (msg.payload in node-red)
+  sink=pulse.sink_list()[int(index)]
+  return jsonpickle.encode(sink)
